741098d0b9d84469f735eb24aaa2fc3b2682c961
md ashikul <ashik.nwu@gmail.com>
2019-12-26 16:11:15 +0600

    asfa

--- day 1/PlaygroundPractis.playground/Contents.swift
+++ day 1/PlaygroundPractis.playground/Contents.swift
@@ -1,36 +1,95 @@
 import UIKit
-//Variable Decliration and print
+
+var str = "Bangladesh"
+str = "India"
+print(str)
+
 let age = 30
-let name = "Karim"
-let ss = name + "Is" + String(age)
+let name = "Abdur Rahman"
+let ss = name + "is" + String(age)
 print(ss)
-let js = name + " is \(age) years old"
-print(js)
 
-//Empty String Check
-
+let a = 10
+let b = 20
+let c = a+b
+print(c)
+// Empty String Check
 let check = ""
-if check.isEmpty{
-    print("Empty Value")
+if check.isEmpty {
+    print("There is no cvalue")
 }else{
-    print("Value is \(check)")
+    print("There is some value")
+}
+// Compare String
+
+var v1 = "Test"
+var v2 = "Test"
+if v1 == v2 {
+    print("Both are same")
+}else{
+    print("Not same")
 }
 
-//Compare String
-let value1 = "Ashik"
-let value2 = "Ashik"
-if value1 == value2 {
-    print("String is Equal")
+// Array declaration
+var arr = [String]()
+arr = ["a","b","C"]
+print(arr[0])
+
+let arr1:[Int] = [1,2,3,4]
+print(arr1)
+
+// Optional
+
+var OptionalNumber:Int?
+print (OptionalNumber)
+
+OptionalNumber = 100
+
+print(OptionalNumber)
+
+// If let
+
+if let number = OptionalNumber {
+    print("I have the value and it is \(number)")
 }else{
-    print("String is not Equal")
+    print("I have no any value")
 }
 
-//Array and Empty Array
+// GUARD LET
 
-var someArray = [Int]()
-someArray.append(5)
-someArray.append(10)
-print(someArray)
-//Remove Array
-someArray.remove(at: 1) // remove Value Position
-print(someArray)
+func dubleNumber(numebr:Int?){
+    guard let number = numebr else{
+        print("Exit function")
+        return
+    }
+    print("The number is \(number*2)")
+}
+dubleNumber(numebr: OptionalNumber)
+
+// Force unwraping
+
+let forcevalue = OptionalNumber!
+print(forcevalue)
+
+// Optional chaining
+
+struct Device{
+    var type: String
+    var price : Float
+    var color: String
+}
+var MyPhone : Device?
+MyPhone = Device(type: "Iphone", price: 999.9, color: "Space Gray")
+
+var YourPhone : Device?
+YourPhone = Device(type: "Android", price: 200, color: "Gray")
+
+let devicePrice = MyPhone?.price
+if let PriceVal = devicePrice{
+    print("Total price is \(PriceVal*3)")
+}
+
+let You = YourPhone?.type
+if let TypeVal = You{
+    print("Your Phone Type \(TypeVal)")
+}
